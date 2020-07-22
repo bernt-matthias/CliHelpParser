dc70e3057b9fd8948c6956df50f9cc441871d75b
Matthias Bernt <m.bernt@ufz.de>
2020-07-22 18:01:55 +0200

    1st running version
    - add galaxyxml requirement
    - some bugfixes

--- acclimatise/converter/__init__.py
+++ acclimatise/converter/__init__.py
@@ -44,7 +44,6 @@ class WrapperGenerator:
         :param type: The type of converter, e.g. 'cwl' or 'wdl'
         """
         for subclass in cls.__subclasses__():
-            print("%s" % subclass)
             if subclass.format() == typ:
                 return subclass
 
